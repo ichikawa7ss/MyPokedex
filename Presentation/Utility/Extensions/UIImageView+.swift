7180533ff55a504ebe6073a2a194d10e8ed9020e
ichikawa7ss <i2s7.chacha@gmail.com>
2020-05-27 12:41:58 +0900

    Nukeを導入し、画像取得のextensionを追加

---
+++ Presentation/Utility/Extensions/UIImageView+.swift
@@ -0,0 +1,29 @@
+//
+//  UIImageView+.swift
+//  Presentation
+//
+//  Created by ichikawa on 2020/05/27.
+//
+
+import UIKit
+import Nuke
+
+extension UIImageView {
+    
+    typealias Completion = (Result<UIImage, Error>) -> Void
+    
+    func loadImage(_ urlString: String, placeholder: UIImage? = nil, completion: Completion? = nil) {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        Nuke.loadImage(with: url, options: ImageLoadingOptions(placeholder: placeholder), into: self) { result in
+            switch result {
+            case .success(let response):
+                completion?(.success(response.image))
+            case .failure(let error):
+                completion?(.failure(error))
+            }
+        }
+    }
+}
